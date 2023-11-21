2d978c3a4f4c83116e310123cbd95dd239f07385
PauloAlecio <46734970+PauloAlecio@users.noreply.github.com>
2023-11-21 15:23:53 -0300

    chore: ADD Aplicacao

---
+++ java-gpt/src/main/java/com/paulo/alecio/javagpt/JavaGptApplication.java
@@ -0,0 +1,14 @@
+package com.paulo.alecio.javagpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGptApplication.class, args);
+
+	}
+
+}
---
+++ java-gpt/src/test/java/com/paulo/alecio/javagpt/JavaGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulo.alecio.javagpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
